f51264360f30435808282d40ba4a4d1a8fc6067a
Eugene Makhinya <eugene.makhinya@gmail.com>
2021-06-15 15:15:20 +0100

    split handler to another package

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -4,13 +4,13 @@ import (
 	"context"
 	"fmt"
 	"log"
-	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
 
 	"github.com/1r0npipe/shortener-web-links/internal/config"
+	"github.com/1r0npipe/shortener-web-links/internal/handler"
 	"github.com/1r0npipe/shortener-web-links/internal/params"
 	"github.com/gin-gonic/gin"
 )
@@ -30,20 +30,21 @@ func main() {
 	signal.Notify(osSig,
 		syscall.SIGINT,
 		syscall.SIGTERM)
+	
 
-	r := gin.Default()
-	r.GET("/healthz", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"status": "OK",
-		})
-	})
+	router := gin.Default()
+	router.GET("/healthz", handler.CheckHealth)
 
-	go r.Run(":" + config.Server.Port)
-
+	go func() {
+		err := router.Run(":" + config.Server.Port)
+		if err != nil {
+			log.Fatal("can't start server")
+		}
+	}()
 	<-osSig
 	log.Printf("shutting down ...\n")
-	_, cancel := context.WithTimeout(context.Background(), time.Duration(config.Server.Timeout)*time.Second)
-	defer cancel()
+	_, cancelFunc := context.WithTimeout(context.Background(), time.Duration(config.Server.Timeout)*time.Second)
+	defer cancelFunc()
 	log.Printf("the service has been down...")
 
 }
